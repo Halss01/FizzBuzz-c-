994699d150b20b543eed3237e72e5a89af7426d5
KarelNakladal <60643873+KarelNakladal@users.noreply.github.com>
2021-04-19 23:41:35 +0200

    fizz buzz

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for( i = 1; i < 101; i++)
+            {
+                int Fizz = i % 3;
+                int Buzz = i % 5;
+                	if(Fizz == 0){
+                        Console.WriteLine("Fizz");
+                    }
+                	else if(Buzz == 0){
+                        Console.WriteLine("Buzz");
+                    }
+                    else{
+                        Console.WriteLine(i);
+                    }
+            }
+        }
+    }
+}
