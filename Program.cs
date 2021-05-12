2de421589a58e19d4e5ffd06656d702d386fdf49
KarelNakladal <karel.nakladal@student.ossp.cz>
2021-05-12 11:52:59 +0200

    update

--- Program.cs
+++ Program.cs
@@ -4,28 +4,35 @@ namespace fizzbuzz
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int i;
-            for( i = 1; i < 101; i++)
-            {
-                int Fizz = i % 3;
-                int Buzz = i % 5;
-
-                if(Fizz == 0 && Buzz ==0){
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if(Fizz == 0){
-                    Console.WriteLine("Fizz");
-                }
-                else if(Buzz == 0){
-                    Console.WriteLine("Buzz");
-                }
-
-                else{
-                    Console.WriteLine(i);
-                }
+        static string unit(int i){
+            int Fizz = i % 3;
+            int Buzz = i % 5;
+                
+            if(Fizz == 0 && Buzz ==0){
+                return "FizzBuzz";
+            }
+            else if(Fizz == 0){
+                return "Fizz";
+            }
+            else if(Buzz == 0){
+                return"Buzz";
+            }
+            else{
+               return  i.ToString();
             }
         }
+
+        static void Main(string[] args)
+        {
+            for(int  i = 1; i < 101; i++)
+            {
+                Console.WriteLine(unit(i));
+
+            }
+        }
+
+
+
     }
+
 }
